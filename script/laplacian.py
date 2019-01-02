6d98629b1d812f81abcfc00b239cf21f20b45c41
Keishiro Kawabata <kawabata.keishiro@gmail.com>
2019-01-03 03:33:31 +0900

    comment out unnecessary parts

--- script/laplacian.py
+++ script/laplacian.py
@@ -7,7 +7,7 @@ from pymatgen.core.structure import Structure
 from pymatgen.analysis.local_env import VoronoiNN
 from pymatgen.analysis.structure_analyzer import VoronoiConnectivity
 from scipy.spatial import Voronoi
-from data_processing import get_bond_indices, calc_pair_wise_properties
+from utils import get_bond_indices, calc_pair_wise_properties
 import warnings
 warnings.filterwarnings("ignore") # ignore RuntimeWarning (division by zero)
 
@@ -54,7 +54,7 @@ def get_adj_matrix(structure, tol=0, cutoff=10):
 
 
 # calculate Laplacian matrix from adjacency matrix
-def get_laplacian(adj_matrix, structure, potential=False):
+def get_laplacian(adj_matrix, structure, potential=True):
     # compute degree matrix
     deg_matrix = np.diag(np.sum(adj_matrix, axis=1))
     # standard Laplacian
@@ -64,8 +64,11 @@ def get_laplacian(adj_matrix, structure, potential=False):
         distance_matrix = structure.distance_matrix
         reciprocal_distance_matrix = distance_matrix ** -1
         reciprocal_distance_matrix[reciprocal_distance_matrix == np.inf] = 0
-        potential_matrix = np.diag(np.sum(reciprocal_distance_matrix, axis=1))
-        laplacian = laplacian - potential_matrix
+        # potential_matrix = np.diag(np.sum(reciprocal_distance_matrix, axis=1))
+        # laplacian = laplacian - potential_matrix
+        for i in range(len(reciprocal_distance_matrix)):
+            reciprocal_distance_matrix[i,i] = 1
+        laplacian = laplacian * reciprocal_distance_matrix
 
     return laplacian
 
@@ -75,20 +78,25 @@ def compute_descriptors_from_laplacian(laplacian):
     w, v = np.linalg.eigh(laplacian)
     # It is sure that the nontrivial smallest eigenvalue index is in the second
     # since all graphs are connected graph.
-    nontrivial_smallest_eigenvalue_index = np.argsort(w)[1]
-    alg_connectivity = w[nontrivial_smallest_eigenvalue_index]
+    # nontrivial_smallest_eigenvalue_index = np.argsort(w)[1]
+    # alg_connectivity = w[nontrivial_smallest_eigenvalue_index]
 
-    return alg_connectivity, np.mean(w), np.std(w)
+    return np.mean(w), np.std(w)
 
 
 # compute Laplacian-based descriptors
-def calc_laplacian_descriptors(atomic_df, POSCAR_path, tol=0, cutoff=10):
+def calc_laplacian_descriptors(atomic_df, POSCAR_path, adj_matrix_list=None,
+                                tol=0, cutoff=10, potential=True):
     structure = set_structure_from_poscar_file(POSCAR_path)
     primitive_structure = structure.get_primitive_structure()
     species = primitive_structure.species
 
-    adj_matrix_no_weight, adj_matrix_multi_edge, adj_matrix_sol_angle = \
+    if adj_matrix_list == None:
+        adj_matrix_no_weight, adj_matrix_multi_edge, adj_matrix_sol_angle = \
                             get_adj_matrix(primitive_structure, tol, cutoff)
+    else:
+        adj_matrix_no_weight, adj_matrix_multi_edge, adj_matrix_sol_angle = \
+                    adj_matrix_list[0], adj_matrix_list[1], adj_matrix_list[2]
 
     bond_indices = get_bond_indices(adj_matrix_no_weight)
     diff_mat = calc_pair_wise_properties(atomic_df, bond_indices, species, 'diff')
@@ -97,22 +105,32 @@ def calc_laplacian_descriptors(atomic_df, POSCAR_path, tol=0, cutoff=10):
     # shape => (n_properties, n_atoms, n_atoms)
     adj_matrix_atomic_multi_edge = diff_mat * adj_matrix_multi_edge
     # shape => (n_properties, n_atoms, n_atoms)
+    adj_matrix_atomic_sol_angle = diff_mat * adj_matrix_sol_angle
 
     adj_matrix_list = [
             adj_matrix_no_weight, adj_matrix_multi_edge, adj_matrix_sol_angle]
     all_adj_matrix_list = adj_matrix_list + \
                         adj_matrix_atomic_weighted.tolist() + \
-                        adj_matrix_atomic_multi_edge.tolist()
-    laplacian_list = [get_laplacian(np.array(x), structure, potential=True)
+                        adj_matrix_atomic_multi_edge.tolist() + \
+                        adj_matrix_atomic_sol_angle.tolist()
+    laplacian_list = [get_laplacian(np.array(x),
+                        primitive_structure, potential=potential)
                             for x in all_adj_matrix_list]
     descriptors = np.array([compute_descriptors_from_laplacian(laplacian)
                             for laplacian in laplacian_list])
     # Even if a graph is weighted, the Fiedler vector is the same with
     # one without weight. Therefore we calculate the std of the Fiedler vector
     # only once.
-    w, v = np.linalg.eigh(laplacian_list[0])
-    nontrivial_smallest_eigenvalue_index = np.argsort(w)[1]
-    std_fiedler_vector = np.std(v[:, nontrivial_smallest_eigenvalue_index])
-    descriptors = np.append(descriptors, std_fiedler_vector)
+    # w, v = np.linalg.eigh(laplacian_list[0])
+    # nontrivial_smallest_eigenvalue_index = np.argsort(w)[0]
+    # mean_fiedler_vector = np.mean(v[:, nontrivial_smallest_eigenvalue_index])
+    # std_fiedler_vector = np.std(v[:, nontrivial_smallest_eigenvalue_index])
+    # nontrivial_smallest_eigenvalue_index2 = np.argsort(w)[1]
+    # mean_fiedler_vector2 = np.mean(v[:, nontrivial_smallest_eigenvalue_index2])
+    # std_fiedler_vector2 = np.std(v[:, nontrivial_smallest_eigenvalue_index2])
+    # descriptors = np.append(descriptors, mean_fiedler_vector)
+    # descriptors = np.append(descriptors, std_fiedler_vector)
+    # descriptors = np.append(descriptors, mean_fiedler_vector2)
+    # descriptors = np.append(descriptors, std_fiedler_vector2)
 
-    return descriptors
+    return descriptors.flatten()
