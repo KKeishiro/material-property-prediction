ccc708a2fbfec04f354abb60051ffe40d5a5309a
Keishiro Kawabata <kawabata.keishiro@gmail.com>
2019-01-03 03:34:29 +0900

    replaced the old one with the new one

--- script/main_laplacian.py
+++ script/main_laplacian.py
@@ -26,28 +26,15 @@ atomic_df = atomic_df.drop(["kai(Allen)", "Rps-d"], axis=1)
 
 parser = argparse.ArgumentParser(description="compute laplacian-based descriptors")
 parser.add_argument("--property", required=True, help="property of a dataset")
-parser.add_argument("--tol", default=0, help="tolerance parameter")
-parser.add_argument("--cutoff", default=10, type=float, help="cutoff value")
 parser.add_argument("--save_path", required=True, help="path to save data")
 parser.add_argument("--isTest", action="store_true", help="whether to test or not")
+parser.add_argument("--potential", action="store_true", help="potential is taken into account")
 args = parser.parse_args()
 
-
-def main(index, property):
-    if property == "cohesive":
-        compound_dir = lines[index].strip()
-        POSCAR_path = os.path.join(descriptors_dir, compound_dir, "POSCAR")
-    elif property == "ltc" or property == "mp":
-        line = lines[index].strip().split()
-        compound_dir = line[0]
-        POSCAR_path = os.path.join(dir_path, compound_dir, "POSCAR")
-
-    # compute descriptors
-    descriptor = calc_laplacian_descriptors(atomic_df, POSCAR_path,
-                                tol=args.tol,
-                                cutoff=args.cutoff)
-    return descriptor, compound_dir
-
+load_dir = os.path.join('data/descriptors', args.property)
+adj_matrix_no_weight = np.load(os.path.join(load_dir, 'adj_matrix_no_weight.npy'))
+adj_matrix_multi_edge = np.load(os.path.join(load_dir, 'adj_matrix_multi_edge.npy'))
+adj_matrix_sol_angle = np.load(os.path.join(load_dir, 'adj_matrix_sol_angle.npy'))
 
 if __name__ == "__main__":
 
@@ -68,16 +55,32 @@ if __name__ == "__main__":
     with open(compounds_list_path) as f:
         lines = f.readlines()
         if args.isTest == True:
-            n_samples = 10
+            n_samples = 20
         else:
             n_samples = len(lines)
 
-        results = Parallel(n_jobs=-1, verbose=2)(
-              delayed(main)(index, args.property) for index in range(n_samples))
-        results = np.array(results)
-        descriptors = results[:,0]
-        descriptors = [descriptor for descriptor in descriptors]
-        compounds_list = results[:,1]
+        descriptors = []
+        compounds_list = []
+        for index in range(n_samples):
+            if args.property == "cohesive":
+                compound_dir = lines[index].strip()
+                POSCAR_path = os.path.join(descriptors_dir, compound_dir, "POSCAR")
+            elif args.property == "ltc" or property == "mp":
+                line = lines[index].strip().split()
+                compound_dir = line[0]
+                POSCAR_path = os.path.join(dir_path, compound_dir, "POSCAR")
+
+            if index % 1000 == 0:
+                print(index)
+            adj_matrix_list = [adj_matrix_no_weight[index],
+                                adj_matrix_multi_edge[index],
+                                adj_matrix_sol_angle[index]]
+            # compute descriptors
+            descriptor = calc_laplacian_descriptors(atomic_df, POSCAR_path,
+                                                    adj_matrix_list,
+                                                    potential=args.potential)
+            descriptors.append(descriptor)
+            compounds_list.append(compound_dir)
 
         print('It took {} sec.'.format(time() - start))
 
