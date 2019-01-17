62659e7611935e05f29f8b506a70bced6584c18a
Keishiro Kawabata <kawabata.keishiro@gmail.com>
2019-01-18 01:46:57 +0900

    removed redundant calculation

--- script/laplacian.py
+++ script/laplacian.py
@@ -12,12 +12,14 @@ import warnings
 warnings.filterwarnings("ignore") # ignore RuntimeWarning (division by zero)
 
 
-valence_dict = {'Li':1, 'Na':1, 'K':1, 'Rb':1, 'Cs':1,
-                'Be':2, 'Mg':2, 'Ca':2, 'Sr':2, 'Ba':2, 'Zn':2, 'Cd':2, 'Hg':2,
-                'Al':3, 'Ga':3, 'In':3, 'Sc':3, 'Y':3, 'La':3,
-                'F':-1, 'Cl':-1, 'Br':-1, 'I':-1,
-                'O':-2, 'S':-2, 'Se':-2, 'Te':-2,
-                'N':-3, 'P':-3, 'As':-3, 'Sb':-3}
+valence_dict = {'H':1,'Li':1,'Na':1,'K':1,'Rb':1,'Cs':1,'Ag':1,
+                'Be':2,'Mg':2,'Ca':2,'Sr':2,'Ba':2,'Zn':2,'Cd':2,'Hg':2,'Pb':2,
+                'B':3,'Al':3,'Ga':3,'In':3,'Sc':3,'Y':3,'La':3,
+                'Si':4,'Pd':4,
+                'F':-1,'Cl':-1,'Br':-1,'I':-1,
+                'O':-2,'S':-2,'Se':-2,'Te':-2,
+                'N':-3,'P':-3,'As':-3,'Sb':-3,
+                'C':-4}
 
 
 def set_structure_from_poscar_file(POSCAR_path):
@@ -61,30 +63,50 @@ def get_adj_matrix(structure, tol=0, cutoff=10):
     return adj_matrix_no_weight, adj_matrix_multi_edge, adj_matrix_sol_angle
 
 
+def calc_potential_mat(adj_matrix, structure, oxi_state_guesses=False):
+    species = structure.species
+    oxi_state = []
+    coulomb_matrix = np.zeros((len(species), len(species)))
+    for i in range(len(species)):
+        for j in range(len(species)):
+            if i < j:
+                # oxi_state is assigned from the prepared dict
+                if not oxi_state_guesses:
+                    coulomb_matrix[i, j] = valence_dict[str(species[i])] * \
+                                            valence_dict[str(species[j])]
+                else: # oxi_state is guessed
+                    if len(oxi_state) == 0:
+                        oxi_states = structure.composition.oxi_state_guesses(
+                                            all_oxi_states=False)
+                        if oxi_states == []:
+                            oxi_states = structure.composition.oxi_state_guesses(
+                                                all_oxi_states=True)
+                        assert oxi_states != [], 'oxi_state could not be guessed'
+                        oxi_state = oxi_states[0]
+                        coulomb_matrix[i, j] = oxi_state[str(species[i])] * \
+                                                oxi_state[str(species[j])]
+            else:
+                coulomb_matrix[i, j] = coulomb_matrix[j, i]
+    distance_matrix = structure.distance_matrix
+    reciprocal_distance_matrix = distance_matrix ** -1
+    reciprocal_distance_matrix[reciprocal_distance_matrix == np.inf] = 0
+    coulomb_matrix = coulomb_matrix * reciprocal_distance_matrix
+    # coulomb_matrix = coulomb_matrix * adj_matrix
+    potential_matrix = np.diag(np.sum(coulomb_matrix, axis=1))
+    print(oxi_state)
+
+    return potential_matrix
+
+
 # calculate Laplacian matrix from adjacency matrix
-def get_laplacian(adj_matrix, structure, potential=True):
+def get_laplacian(adj_matrix, structure, potential=None):
     # compute degree matrix
     deg_matrix = np.diag(np.sum(adj_matrix, axis=1))
     # standard Laplacian
     laplacian = deg_matrix - adj_matrix
     # generalized laplacian (with potential)
-    if potential == True:
-        species = structure.species
-        coulomb_matrix = np.zeros((len(species), len(species)))
-        for i in range(len(species)):
-            for j in range(len(species)):
-                if i < j:
-                    coulomb_matrix[i, j] = valence_dict[str(species[i])] * \
-                                            valence_dict[str(species[j])]
-                else:
-                    coulomb_matrix[i, j] = coulomb_matrix[j, i]
-        distance_matrix = structure.distance_matrix
-        reciprocal_distance_matrix = distance_matrix ** -1
-        reciprocal_distance_matrix[reciprocal_distance_matrix == np.inf] = 0
-        coulomb_matrix = coulomb_matrix * reciprocal_distance_matrix
-        potential_matrix = np.diag(np.sum(coulomb_matrix, axis=1))
-        # potential_matrix = np.diag(np.sum(reciprocal_distance_matrix, axis=1))
-        laplacian = laplacian + potential_matrix
+    if len(potential) > 0:
+        laplacian = laplacian + potential
 
     return laplacian
 
@@ -102,7 +124,7 @@ def compute_descriptors_from_laplacian(laplacian):
 
 # compute Laplacian-based descriptors
 def calc_laplacian_descriptors(atomic_df, POSCAR_path, adj_matrix_list=None,
-                                tol=0, cutoff=10, potential=True):
+                    tol=0, cutoff=10, potential=True, oxi_state_guesses=False):
     structure = set_structure_from_poscar_file(POSCAR_path)
     primitive_structure = structure.get_primitive_structure()
     species = primitive_structure.species
@@ -129,9 +151,16 @@ def calc_laplacian_descriptors(atomic_df, POSCAR_path, adj_matrix_list=None,
                         adj_matrix_atomic_weighted.tolist() + \
                         adj_matrix_atomic_multi_edge.tolist() + \
                         adj_matrix_atomic_sol_angle.tolist()
+
+    potential_matrix = []
+    if potential:
+        potential_matrix = calc_potential_mat(adj_matrix_multi_edge,
+                                            primitive_structure,
+                                            oxi_state_guesses=oxi_state_guesses)
     laplacian_list = [get_laplacian(np.array(x),
-                        primitive_structure, potential=potential)
-                            for x in all_adj_matrix_list]
+                                    primitive_structure,
+                                    potential=potential_matrix)
+                                    for x in all_adj_matrix_list]
     descriptors = np.array([compute_descriptors_from_laplacian(laplacian)
                             for laplacian in laplacian_list])
     # Even if a graph is weighted, the Fiedler vector is the same with
